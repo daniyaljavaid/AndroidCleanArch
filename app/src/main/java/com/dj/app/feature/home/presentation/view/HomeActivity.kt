118da0bc9ccf3c65a6bee95d3f76c10a4703aa88
Daniyal Javaid <daniyal.javaid@greenlightlabs.tech>
2022-01-25 11:31:01 +0500

    home activity with bottom nav setup

---
+++ app/src/main/java/com/dj/app/feature/home/presentation/view/HomeActivity.kt
@@ -0,0 +1,61 @@
+package com.dj.app.feature.home.presentation.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.dj.app.R
+import com.dj.app.core.base.BaseActivity
+import com.dj.app.databinding.ActivityHomeBinding
+import com.dj.app.feature.home.pages.Fragment1
+import com.dj.app.feature.home.pages.Fragment2
+import com.dj.app.feature.home.pages.Fragment3
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        binding.bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    loadFragment(Fragment1())
+                    true
+                }
+                R.id.settings -> {
+                    loadFragment(Fragment2())
+                    true
+                }
+
+                R.id.notifications -> {
+                    loadFragment(Fragment3())
+                    true
+                }
+                else -> false
+            }
+        }
+        loadFragment(Fragment1())
+
+    }
+
+    override fun onBackPressed() {
+        if (binding.bottomNav.selectedItemId == R.id.home) {
+            super.onBackPressed()
+        } else {
+            binding.bottomNav.selectedItemId = R.id.home
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainer.id, fragment)
+            .commit()
+    }
+}
--- app/src/main/java/com/dj/app/feature/login/presentation/view/LoginActivity.kt
+++ app/src/main/java/com/dj/app/feature/login/presentation/view/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.dj.app.feature.login.presentation.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.dj.app.core.base.BaseActivity
 import com.dj.app.databinding.ActivityLoginBinding
+import com.dj.app.feature.home.presentation.view.HomeActivity
 import com.dj.app.feature.login.presentation.viewModel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -31,6 +34,10 @@ class LoginActivity : BaseActivity() {
             viewModel.loginState.collectLatest {
                 if (it.isSuccessful) {
                     Timber.e("Successful")
+                    delay(1500)
+                    val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+                    startActivity(intent)
+                    finish()
                 }
             }
         }

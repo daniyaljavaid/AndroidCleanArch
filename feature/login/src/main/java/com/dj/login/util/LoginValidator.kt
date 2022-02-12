0d2b8e7d56836224af6821b245dbfa511c5858e3
Muhammad Daniyal Javaid <daniyal.javaid@greenlightlabs.tech>
2022-02-12 12:27:24 +0500

    feature login - password unit tests

--- feature/login/src/main/java/com/dj/login/util/LoginValidator.kt
+++ feature/login/src/main/java/com/dj/login/util/LoginValidator.kt
@@ -3,7 +3,14 @@ package com.dj.login.util
 import java.util.regex.Pattern
 
 class LoginValidator {
-    fun isEmailValid(email: String): Boolean =
+    /*
+        Contains:
+            username
+            domain
+            2 character domain
+            country domain
+    */
+    fun isEmailValid(email: String) =
         Pattern.compile(
             "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
                     "\\@" +
@@ -13,4 +20,15 @@ class LoginValidator {
                     "[a-zA-Z0-9][a-zA-Z0-9\\-]{1,25}" +
                     ")+"
         ).matcher(email).matches()
+
+    /*
+        At least 1 alphabet
+        At least 1 digit
+        No space
+        At least 1 special characters e.g. @$!%*#?&^_-
+        Minimum 8 characters long
+    */
+    fun isPasswordValid(password: String) = Pattern.compile(
+        "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])([a-zA-Z0-9@$!%*?&]{8,})"
+    ).matcher(password).matches()
 }
--- feature/login/src/test/java/com/dj/login/util/LoginValidatorTest.kt
+++ feature/login/src/test/java/com/dj/login/util/LoginValidatorTest.kt
@@ -45,8 +45,58 @@ class LoginValidatorTest {
     }
 
     @Test
-    fun `valid email with 2 digit domain returns true`() {
+    fun `valid email with 2 character domain returns true`() {
         assertEquals(true, validator.isEmailValid("abc@gmail.co.pk"))
     }
 
+    //Password Tests
+    @Test
+    fun `empty password returns false`() {
+        assertEquals(false, validator.isPasswordValid(""))
+    }
+
+    @Test
+    fun `password without alphabet returns false`() {
+        assertEquals(false, validator.isPasswordValid("123213@._"))
+    }
+
+    @Test
+    fun `password without digit returns false`() {
+        assertEquals(false, validator.isPasswordValid("Aasdasd@._"))
+    }
+
+    @Test
+    fun `password with whitespace returns false`() {
+        assertEquals(false, validator.isPasswordValid("Aasdasd 123._"))
+    }
+
+    @Test
+    fun `password with 6 characters returns false`() {
+        assertEquals(false, validator.isPasswordValid("Test@1"))
+    }
+
+    @Test
+    fun `password without cap alphabet returns false`() {
+        assertEquals(false, validator.isPasswordValid("a123._asd"))
+    }
+
+    @Test
+    fun `password without special character returns false`() {
+        assertEquals(false, validator.isPasswordValid("Test1234"))
+    }
+
+    @Test
+    fun `password with 1cap alphabet, 1digit, no space, 8 char with special char returns true`() {
+        assertEquals(true, validator.isPasswordValid("Test@123"))
+    }
+
+    @Test
+    fun `password with 2cap alphabet, 3digit, no space, 10 char with 3 special char returns true`() {
+        assertEquals(true, validator.isPasswordValid("*TesT@123*"))
+    }
+
+    @Test
+    fun `password with no cap alphabet, 3digit, no space, 10 char with 3 special char returns true`() {
+        assertEquals(false, validator.isPasswordValid("*test@123*"))
+    }
 }

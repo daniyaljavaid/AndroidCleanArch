3ce23191b238107c1b51e5d2634047a06b502bad
daniyaljavaid <daniyaljavaid95@gmail.com>
2022-04-21 01:47:00 +0500

    splash screen added + app icon fixed

--- feature/login/src/main/java/com/dj/login/presentation/view/LoginActivity.kt
+++ feature/login/src/main/java/com/dj/login/presentation/view/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.dj.login.presentation.view
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -23,6 +24,7 @@ class LoginActivity : BaseActivity() {
     private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)

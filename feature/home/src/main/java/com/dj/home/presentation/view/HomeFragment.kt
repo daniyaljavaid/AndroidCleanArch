28ba47221800166426fb58bb31367c1ca1aa7615
daniyaljavaid <daniyaljavaid95@gmail.com>
2022-04-23 19:48:47 +0500

    navigate between login to home module

--- feature/home/src/main/java/com/dj/home/presentation/view/HomeFragment.kt
+++ feature/home/src/main/java/com/dj/home/presentation/view/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.dj.home.pages
+package com.dj.home.presentation.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,18 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.dj.home.R
 
-class Fragment2 : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
 }
--- feature/login/src/main/java/com/dj/login/presentation/view/LoginFragment.kt
+++ feature/login/src/main/java/com/dj/login/presentation/view/LoginFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.NavDeepLinkRequest
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import com.dj.core.base.BaseFragment
+import com.dj.login.R
 import com.dj.login.databinding.FragmentLoginBinding
 import com.dj.login.presentation.state.LoginState
 import com.dj.login.presentation.viewModel.LoginViewModel
@@ -46,13 +51,14 @@ class LoginFragment : BaseFragment() {
                     when (state) {
                         is LoginState.Success -> {
                             delay(1500)
-//                            val intent = Intent()
-//                            intent.setClassName(
-//                                this@LoginActivity,
-//                                "com.dj.home.presentation.view.HomeActivity"
-//                            )
-//                            startActivity(intent)
-//                            finish()
+                            //implicit deeplink
+                            val request = NavDeepLinkRequest.Builder
+                                .fromUri("my-app://feature.home/home_fragment".toUri())
+                                .build()
+                            val options =
+                                NavOptions.Builder().setPopUpTo(R.id.loginFragment, true).build()
+
+                            findNavController().navigate(request, options)
                         }
 
                     }
